f234d5a510b3ab81c903b3e3f7d9c314ef5f88ec
jatinsharma9871 <jatinsharma9871@gmail.com>
2022-07-07 18:11:11 +0530

    update

--- src/modules/utils/testimonial.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-const urlTestimonial = process.env.REACT_APP_HOST + '/testimonial'
-
-export const listTestimonialHome = (params) => {
-    const urlTestimonialHome = urlTestimonial + `?limit=${params.limit}&page=${params.page}`
-    return axios.get(urlTestimonialHome)
-}
